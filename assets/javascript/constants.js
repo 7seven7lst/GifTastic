f5f20f832a53ff7da5a60715a798cdb2a5a172e0
Shengtao Li <sevenlist0110@gmail.com>
2017-03-11 17:08:31 -0800

    [fix] fix https error

--- assets/javascript/constants.js
+++ assets/javascript/constants.js
@@ -3,6 +3,6 @@ const $addTopicButton = $("#add-topic");
 const $giphyViewDiv = $("#giphy-view");
 const $inputBox = $("#topic-input");
 const GIPHY_PUBLIC_KEY = "dc6zaTOxFJmzC";
-const GIPHY_API_BASE_URL = "http://api.giphy.com/v1/gifs";
+const GIPHY_API_BASE_URL = "https://api.giphy.com/v1/gifs";
 const GIPHY_QUERY_LIMIT = 10;
 const GIPHY_RATING = "g";
